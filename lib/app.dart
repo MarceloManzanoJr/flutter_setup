bef0625cdf61c028f70dc9629b8ec21ad7d282cf
MarceloManzanoJr <148688390+MarceloManzanoJr@users.noreply.github.com>
2026-03-08 23:29:13 +0800

    login finish

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:innolab_application/features/authentication/screens/admin_login_screen.dart';
 import 'package:innolab_application/utils/theme/theme.dart';
 
 class App extends StatelessWidget {
@@ -10,6 +11,7 @@ class App extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: ATheme.lightTheme,
       darkTheme: ATheme.darkTheme,
+      home: const AdminLoginScreen(),
     );
   }
 }
